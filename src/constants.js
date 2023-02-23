c3a7a5534de262f84de24e5002d9c5d16170caca
Jocker34 <kacper2162@wp.pl>
2023-02-23 18:01:11 +0100

    Add params to RTK query

--- src/constants.js
+++ src/constants.js
@@ -11,5 +11,5 @@ export const ALT = {
 export const API = 'https://api.themoviedb.org/3';
 export const YOUTUBE = 'https://www.youtube.com/embed/';
 
-export const API_KEY = '?api_key=c0e7b20ce2dba89c12ce2c123de59994';
+export const API_KEY = 'c0e7b20ce2dba89c12ce2c123de59994';
 export const IMAGE = 'https://image.tmdb.org/t/p/w500';
--- src/services/endpoints/movies.builder.js
+++ src/services/endpoints/movies.builder.js
@@ -5,74 +5,92 @@ export const moviesEndpoints = movieApi.injectEndpoints({
   endpoints: (builder) => ({
     nowPlaying: builder.query({
       query: (page) => ({
-        url: `movie/now_playing${API_KEY}&page=${page}`,
+        url: `movie/now_playing`,
+        params: { api_key: API_KEY, page: page },
       }),
     }),
     popular: builder.query({
       query: (page) => ({
-        url: `movie/popular${API_KEY}&page=${page}`,
+        url: `movie/popular`,
+        params: { api_key: API_KEY, page: page },
       }),
     }),
     topRated: builder.query({
       query: (page) => ({
-        url: `movie/top_rated${API_KEY}&page=${page}`,
+        url: `movie/top_rated`,
+        params: { api_key: API_KEY, page: page },
       }),
     }),
     upComing: builder.query({
       query: (page) => ({
-        url: `movie/upcoming${API_KEY}&&page=${page}`,
+        url: `movie/upcoming`,
+        params: { api_key: API_KEY, page: page },
       }),
     }),
     search: builder.query({
       query: (name) => ({
-        url: `/search/movie${API_KEY}&query=${name}`,
+        url: `/search/movie`,
+        params: { api_key: API_KEY, query: name },
       }),
     }),
     video: builder.query({
       query: (id) => ({
-        url: `/movie/${id}/videos${API_KEY}`,
+        url: `/movie/${id}/videos`,
+        params: { api_key: API_KEY },
       }),
     }),
     topBilledCast: builder.query({
       query: (id) => ({
-        url: `/movie/${id}/credits${API_KEY}`,
+        url: `/movie/${id}/credits`,
+        params: { api_key: API_KEY },
       }),
     }),
     movieImages: builder.query({
       query: (id) => ({
-        url: `/movie/${id}/images${API_KEY}`,
+        url: `/movie/${id}/images`,
+        params: { api_key: API_KEY },
       }),
     }),
     movieDetails: builder.query({
       query: (args) => {
         const { id, lang } = args;
-        return { url: `/movie/${id}${API_KEY}&language=${lang}` };
+        return {
+          url: `/movie/${id}`,
+          params: { api_key: API_KEY, language: lang },
+        };
       },
     }),
     movieRecommendations: builder.query({
       query: (id) => ({
-        url: `/movie/${id}/recommendations${API_KEY}`,
+        url: `/movie/${id}/recommendations`,
+        params: { api_key: API_KEY },
       }),
     }),
     personKnownBy: builder.query({
       query: (id) => ({
-        url: `/person/${id}/movie_credits${API_KEY}`,
+        url: `/person/${id}/movie_credits`,
+        params: { api_key: API_KEY },
       }),
     }),
     personDetails: builder.query({
       query: (args) => {
         const { id, lang } = args;
-        return { url: `/person/${id}${API_KEY}&language=${lang}` };
+        return {
+          url: `/person/${id}`,
+          params: { api_key: API_KEY, language: lang },
+        };
       },
     }),
     personImages: builder.query({
       query: (id) => ({
-        url: `/person/${id}/images${API_KEY}`,
+        url: `/person/${id}/images`,
+        params: { api_key: API_KEY },
       }),
     }),
     movieGenres: builder.query({
       query: () => ({
-        url: `/genre/movie/list${API_KEY}`,
+        url: `/genre/movie/list`,
+        params: { api_key: API_KEY },
       }),
     }),
   }),
